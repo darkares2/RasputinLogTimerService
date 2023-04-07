31ec064898a135ef860604da0d58aed6fc17e8bc
rasputin <thomas.1.andersen@gmail.com>
2023-04-07 14:05:31 +0200

    Mkae sure the logs from DLQ are unique

--- LogTimerService/DLQHelper.cs
+++ LogTimerService/DLQHelper.cs
@@ -12,10 +12,17 @@ namespace Rasputin.LogTimerService
             var current = message.Headers.FirstOrDefault(x => x.Name.Equals("current-queue-header"));
             current.Fields["Name"] = current.Fields["Name"] + $"-Error: {reason}";
             var idHeader = message.Headers.FirstOrDefault(x => x.Name.Equals("id-header"));
+            Random random = new Random();
+
+            var sentTimestamp = current.Fields.ContainsKey("Timestamp") ? DateTime.ParseExact(current.Fields["Timestamp"], "yyyy-MM-ddTHH:mm:ss.fffZ", CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal | DateTimeStyles.AdjustToUniversal) : new DateTime(621355968000000000L, DateTimeKind.Utc);
+            // Since this dead-letter-queue item could overwrite important already logged errors we change the signature 
+            int randomMillisecond = random.Next(1, 1000);
+            sentTimestamp = sentTimestamp.AddMilliseconds(randomMillisecond);
+
             return new LogTimer() {
                 Id = Guid.Parse(idHeader.Fields["GUID"]),
                 Queue = current.Fields["Name"],
-                SentTimestamp = current.Fields.ContainsKey("Timestamp") ? DateTime.ParseExact(current.Fields["Timestamp"], "yyyy-MM-ddTHH:mm:ss.fffZ", CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal | DateTimeStyles.AdjustToUniversal) : new DateTime(621355968000000000L, DateTimeKind.Utc),
+                SentTimestamp = sentTimestamp,
                 ReceiveTimestamp = DateTime.UtcNow,
                 ProcesMs = 0
             };
