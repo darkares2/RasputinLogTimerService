d0a35ac59377ab58b1dcc2788dfd2ad683393ed0
rasputin <thomas.1.andersen@gmail.com>
2023-04-07 11:53:22 +0200

    Use new expaneded table and support update

--- LogTimerService/QueueTriggerLogTimerService.cs
+++ LogTimerService/QueueTriggerLogTimerService.cs
@@ -27,21 +27,37 @@ namespace Rasputin.LogTimerService
 
         private async Task InsertLogTimerAsync(LogTimer logTimer, ILogger log)
         {
-            var str = Environment.GetEnvironmentVariable("sqldb_connection");
-            string query = "INSERT INTO LogTimer (Id, Queue, SentTimestamp, ReceiveTimestamp) VALUES (@Id, @Queue,@SentTimestamp,@ReceiveTimestamp)";
-            using (SqlConnection connection = new SqlConnection(str))
+            var connectionString = Environment.GetEnvironmentVariable("sqldb_connection");
+            string query = @"
+                    MERGE LogTimer AS target
+                    USING (VALUES (@Id, @Queue, @SentTimestamp, @ReceiveTimestamp, @ProcessMs)) AS source (Id, Queue, SentTimestamp, ReceiveTimestamp, ProcessMs)
+                    ON target.Id = source.Id AND target.SentTimestamp = source.SentTimestamp
+                    WHEN MATCHED THEN
+                        UPDATE SET target.Queue = source.Queue, target.ReceiveTimestamp = source.ReceiveTimestamp, target.ProcessMs = source.ProcessMs
+                    WHEN NOT MATCHED THEN
+                        INSERT (Id, Queue, SentTimestamp, ReceiveTimestamp, ProcessMs)
+                        VALUES (source.Id, source.Queue, source.SentTimestamp, source.ReceiveTimestamp, source.ProcessMs);";
+            using (SqlConnection connection = new SqlConnection(connectionString))
             {
+                // open the connection
                 connection.Open();
-                using (var command = new SqlCommand(query, connection))
-                {
-                    command.Parameters.AddWithValue("@ID", logTimer.Id);
-                    command.Parameters.AddWithValue("@Queue", logTimer.Queue);
-                    command.Parameters.AddWithValue("@SentTimestamp", logTimer.SentTimestamp);
-                    command.Parameters.AddWithValue("@ReceiveTimestamp", logTimer.ReceiveTimestamp);
-                    var countInserted = await command.ExecuteNonQueryAsync();
-                    log.LogDebug($"Inserted into LogTimer[{countInserted}]");
-                }
+
+                // create a SqlCommand object for the merge statement
+                SqlCommand command = new SqlCommand(query, connection);
+
+                // add parameters to the SqlCommand object
+                command.Parameters.AddWithValue("@Id", logTimer.Id);
+                command.Parameters.AddWithValue("@Queue", logTimer.Queue);
+                command.Parameters.AddWithValue("@SentTimestamp", logTimer.SentTimestamp);
+                command.Parameters.AddWithValue("@ReceiveTimestamp", logTimer.ReceiveTimestamp);
+                command.Parameters.AddWithValue("@ProcessMs", logTimer.ProcesMs);
+
+                // execute the merge statement
+                int rowsAffected = command.ExecuteNonQuery();
+
+                log.LogDebug($"Inserted into LogTimer[{rowsAffected}]");
             }
+
         }
     }
 }
